545a3a64ce610dbd555890d2494d32323a62d7f6
Jovani Martinez <armando.martinez.1738@gmail.com>
2020-03-27 17:01:15 -0600

    header concluido

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Juegos</title>
+</head>
+<body>
+    <header class="header">
+    <h1>Video Juegos</h1>
+    </header>
+</body>
+</html>
