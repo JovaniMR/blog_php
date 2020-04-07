7f6c2bf7b761c0e2fb881e86255c7c7a8c3cabd5
Jovani Martinez <armando.martinez.1738@gmail.com>
2020-04-06 19:49:23 -0500

    Complementaciión del Readme y añadi estructura de la BD del proyecto

---
+++ includes/categoria.php
@@ -0,0 +1,43 @@
+<!-- Visualizar las entradas por categoria -->
+
+<?php require_once 'conexion.php' ?>
+<?php require_once '../funciones.php' ?>
+
+<?php require_once 'cabecera.php' ?>
+
+
+<div class="container-fluid">
+
+    <div class="main">
+
+        <?php 
+        
+        if(isset($_GET)){
+            $id= (int) $_GET['id'];
+        }
+    
+        $entradas = consultarEntradasCategoria($conexion,$id);
+    if(!empty($entradas)) :
+        while($entrada=mysqli_fetch_assoc($entradas)) :?>
+
+        <article class="card mt-5" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title text-center"><?= $entrada['titulo']?></h2>
+                <h4 class="card-subtitle mb-2 text-muted"> <?=$entrada['nombre'] ?></h4>
+                <span class="badge badge-success"><?=$entrada['categoria']?></span>
+                <span class="badge badge-primary "><?= $entrada['fecha']?></span>
+                <p class="card-text text-justify"> <?= substr($entrada['descripcion'],0,150);?> ...</p>
+            </div>
+        </article>
+        <?php 
+        endwhile;
+    else:
+        ?>
+   
+    </div>
+    <h2 class="text-center mt-5">NO HAY ENTRADAS EN ESTA CATEGORIA</h2>
+    <?php endif?>
+</div>
+
+
+<?php require_once 'footer.php';
---
+++ includes/total_entradas.php
@@ -0,0 +1,43 @@
+<!-- Enlista todas las entradas -->
+
+
+<?php require_once 'conexion.php' ?>
+<?php require_once '../funciones.php' ?>
+
+<?php require_once 'cabecera.php' ?>
+
+
+<div class="container-fluid">
+    <div class="main">
+
+        <?php 
+        
+        $entradas = consultarEntradasTotales($conexion);
+    if(!empty($entradas)) :
+        while($entrada=mysqli_fetch_assoc($entradas)) :?>
+
+        <article class="card mt-5" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title text-center"><?= $entrada['titulo']?></h2>
+                <h4 class="card-subtitle mb-2 text-muted"> <?=$entrada['nombre'] ?></h4>
+                <span class="badge badge-success"><?=$entrada['categoria']?></span>
+                <span class="badge badge-primary "><?= $entrada['fecha']?></span>
+                <p class="card-text text-justify"> <?= substr($entrada['descripcion'],0,150);?> ...</p>
+            </div>
+        </article>
+        <?php 
+        endwhile;
+    endif;
+
+    ?>
+    </div>
+</div>
+
+<div class="entradas d-flex justify-content-center">
+    <a type="submit" class="btn btn-primary mt-5 " href="index.php">Ver menos</a>
+</div>
+
+
+
+
+<?php require_once 'footer.php';
